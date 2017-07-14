fab4394620dc6a430e2f5223a5e4ca57654aad19
Djalel Chefrour <cdjalel@gmail.com>
2017-07-14 03:08:08 +0100

    Rework notification and add vibration

--- app/src/main/java/org/linuxac/bilal/receivers/AlarmReceiver.java
+++ app/src/main/java/org/linuxac/bilal/receivers/AlarmReceiver.java
@@ -24,6 +24,9 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
@@ -33,34 +36,39 @@ import org.linuxac.bilal.AthanService;
 import org.linuxac.bilal.R;
 import org.linuxac.bilal.activities.MainActivity;
 import org.linuxac.bilal.activities.StopAthanActivity;
+import org.linuxac.bilal.helpers.PrayerTimes;
 import org.linuxac.bilal.helpers.UserSettings;
+
+import static android.content.Context.VIBRATOR_SERVICE;
 
 public class AlarmReceiver extends BroadcastReceiver
 {
     protected static final String TAG = "AlarmReceiver";
-    public static final String PRAYER_INDEX = "org.linuxac.bilal";
+    public static final String EXTRA_PRAYER_INDEX = "org.linuxac.bilal";
 
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        String alarmTxt = intent.getStringExtra(AlarmScheduler.ALARM_TXT);
+        String alarmTxt = intent.getStringExtra(EXTRA_PRAYER_INDEX);
         Log.i(TAG, "Athan alarm is ON: " + alarmTxt);
 
         int index = Integer.parseInt(alarmTxt.substring(0,1));          // 1st char = prayer index
 
+        if (UserSettings.isVibrateEnabled(context)) {
+            // this is independent of notification setVibrate
+            Vibrator vibrator = (Vibrator)context.getSystemService(VIBRATOR_SERVICE);
+            vibrator.vibrate(new long[] {1000, 1000, 1000, 1000, 1000, 1000, 1000}, -1);
+        }
+
         if (UserSettings.isAthanEnabled(context)) {
+            // don't use notification setSound as system can stop and we can't!
             Intent audioIntent = new Intent(context, AthanService.class);
-            audioIntent.putExtra(PRAYER_INDEX, index);
+            audioIntent.putExtra(EXTRA_PRAYER_INDEX, index);
             context.startService(audioIntent);
         }
 
         if (UserSettings.isNotificationEnabled(context)) {
-            String notificationTxt =  alarmTxt.substring(1);
-            showNotification(context, notificationTxt);
-        }
-
-        if (UserSettings.isVibrateEnabled(context)) {
-            // TODO: vibrate
+            showNotification(context, index);
         }
 
         // Broadcast to MainActivity so it updates its screen if on
@@ -71,27 +79,46 @@ public class AlarmReceiver extends BroadcastReceiver
         AlarmScheduler.updatePrayerTimes(context, false);
     }
 
-    private void showNotification(Context context, String contentTxt)
+    private void showNotification(Context context, int index)
     {
         int notificationId = 0;
 
         // Use one intent to show MainActivity and another intent to stop athan (by notification
-        // button or swipe left, TODO add stop from volume down too)
+        // button or swipe left or volume button press)
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent activity = PendingIntent.getActivity(context, 0, intent, 0);
         PendingIntent stopAudioIntent = StopAthanActivity.getIntent(notificationId, context);
 
+        Bitmap largeIconBmp = BitmapFactory.decodeResource(context.getResources(),
+                R.drawable.ic_notif_large);
+        /*Keep this in case we need it
+        Resources res = context.getResources();
+        int height = (int) res.getDimension(android.R.dimen.notification_large_icon_height);
+        int width = (int) res.getDimension(android.R.dimen.notification_large_icon_width);
+        largeIconBmp = Bitmap.createScaledBitmap(largeIconBmp, width, height, false);*/
+
+
+        String contentTitle = String.format(context.getString(R.string.time_for),
+                PrayerTimes.getName(context, index));
+        String contentTxt = String.format(context.getString(R.string.time_in),
+                AlarmScheduler.sCityName); // TODO UserSetting for city name
         String actionTxt = context.getString(R.string.stop_athan);
+
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle(context.getString(R.string.time_for))
+                        .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                        .setSmallIcon(R.drawable.ic_notif)
+                        .setContentTitle(contentTitle)
                         .setContentText(contentTxt)
                         .setContentIntent(activity)
                         .setCategory(NotificationCompat.CATEGORY_ALARM)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
                         .setAutoCancel(true)
                         .setDeleteIntent(stopAudioIntent)
-                        .addAction(R.drawable.ic_clear, actionTxt, stopAudioIntent); //  XML vector
+                        .setLargeIcon(largeIconBmp)
+                        .addAction(R.drawable.ic_stop_athan, actionTxt, stopAudioIntent);
+
+        // TODO: add a timeout (till Iqama) with android O
 
         // Get an instance of the NotificationManager service
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
